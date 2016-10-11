28e52a6632b1ac00639b2fee1d91747a5ac1ace2
Dung Tran <dungtranweb@gmail.com>
2016-10-11 17:37:15 +0700

    Complete 2 pages: Login and Lock Screen

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Login
+$(document).ready(function () {
+    $('.input-login').focusin(function () {
+        $(this).parent().addClass('focus-login');
+        $(this).parent().find('.user').attr('src', '../images/login/user-hover.png');
+        $(this).parent().find('.pass').attr('src', '../images/login/pass-hover.png');
+    });
+    $('.input-login').focusout(function () {
+        $(this).parent().removeClass('focus-login');
+        $(this).parent().find('.user').attr('src', '../images/login/user.png');
+        $(this).parent().find('.pass').attr('src', '../images/login/pass.png');
+    })
+})
